f4a4dd679bd87dee6f560d3f4ccffb50c976434f
CampoJean-Guillaume <jg.campo31@gmail.com>
2024-07-10 12:50:29 +0200

    chore: Update Hero and Status components with revised project details and links

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 import { Baniere } from './_components/Baniere';
 import { Header } from './_components/Header';
 import { Hero } from './_components/Hero';
@@ -7,18 +11,32 @@ import { Spacing } from './_components/Spacing';
 import { Status } from './_components/Status';
 
 export default function Home() {
+  useEffect(() => {
+    // Initialisation de AOS (Animate On Scroll)
+    AOS.init({
+      duration: 2000, // durée de l'animation en millisecondes
+      once: true, // si l'animation doit se produire qu'une seule fois
+    });
+  }, []);
+
   return (
     <main>
       {/* header  is now a top-level component */}
       <Header />
       <Spacing size='sm' />
-      <Hero />
+      <div data-aos='zoom-in' id='hero'>
+        <Hero />
+      </div>
       <Spacing size='sm' />
       <Status />
       <Spacing size='sm' />
-      <Skills />
+      <div data-aos='zoom-in' id='hero'>
+        <Skills />
+      </div>
       <Spacing size='sm' />
-      <SoftSkills />
+      <div data-aos='zoom-in' id='hero'>
+        <SoftSkills />
+      </div>
       <Spacing size='sm' />
       <Baniere />
       <Spacing size='sm' />
